390a2157af9a9d936a2cdf6cbd22a01b8b03e811
nickY <nickyang0501+work@gmail.com>
2022-03-11 22:49:10 +1000

    try something new

--- src/pages/api/followers.ts
+++ src/pages/api/followers.ts
@@ -1,8 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import chromium from 'chrome-aws-lambda';
-import playwright, { Page } from 'playwright-core';
+import puppeteer, { Page } from 'puppeteer';
 
 import type { FollowerInfo } from '_types';
+
+const setPageUserAgent = async (page: Page) => {
+  // set user agent (override the default headless User Agent)
+  await page.setUserAgent(
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4889.0 Safari/537.36',
+  );
+};
 
 const getNumberOfFollowersFromPage = async (page: Page): Promise<number> => {
   // Wait for profile page header load
@@ -36,51 +42,37 @@ const getUsernamesFromFollowersList = async (page: Page) => {
   return followerUsernames;
 };
 
-const loadFullFollowersList = async (
-  page: Page,
-  totalNumberOfFollowers: number,
-): Promise<boolean> => {
+const loadFullFollowersList = async (page: Page): Promise<boolean> => {
   try {
-    // Get the current list length
-    const currentFollowersListLength = await page.evaluate(
-      () =>
-        document.querySelectorAll('div[aria-label="Followers"] ul li').length,
-    );
-    console.log('currentFollowersListLength', currentFollowersListLength);
-    // Load more if current list length < full list length
-    if (currentFollowersListLength < totalNumberOfFollowers) {
-      console.log('load more');
-      // Scroll list to bottom to load more
-      await page.evaluate(() => {
-        const followersListContainer = document.querySelector(
-          'div[aria-label="Followers"] > div > div > div:nth-child(2)',
-        );
-        if (followersListContainer) {
-          followersListContainer.scrollTop =
-            followersListContainer.scrollHeight;
-        }
-      });
-      // Wait for GET /followers API is called
-      try {
-        // If /followers API is not called in 3 seconds, it means list is at bottom
-        // Although it may not match with the Instagram followers count
-        await page.waitForRequest(
-          (request) =>
-            request.url().includes('/followers') && request.method() === 'GET',
-          { timeout: 3000 },
-        );
-      } catch {
-        return Promise.resolve(true);
-      }
-      // Wait for POST /show_many API is called
-      await page.waitForResponse(
-        (response) =>
-          response.url().includes('/show_many') && response.status() === 200,
+    console.log('load more');
+    // Scroll list to bottom to load more
+    await page.evaluate(() => {
+      const followersListContainer = document.querySelector(
+        'div[aria-label="Followers"] > div > div > div:nth-child(2)',
       );
-      return loadFullFollowersList(page, totalNumberOfFollowers);
+      if (followersListContainer) {
+        followersListContainer.scrollTop = followersListContainer.scrollHeight;
+      }
+    });
+    // Wait for GET /followers API is called
+    try {
+      // If /followers API is not called in 3 seconds, it means list is at bottom
+      // Although it may not match with the Instagram followers count
+      await page.waitForRequest(
+        (request) =>
+          request.url().includes('/followers') && request.method() === 'GET',
+        { timeout: 3000 },
+      );
+    } catch {
+      return Promise.resolve(true);
     }
-
-    return Promise.resolve(true);
+    // Wait for POST /show_many API is called
+    await page.waitForResponse(
+      (response) =>
+        response.url().includes('/show_many') && response.status() === 200,
+    );
+    await page.waitForTimeout(1000);
+    return loadFullFollowersList(page);
   } catch (error) {
     console.log(error);
     return Promise.resolve(false);
@@ -93,12 +85,12 @@ const getFollowersFromInstagram = async (
   const followersInfo: FollowerInfo[] = [];
 
   // Init puppeteer
-  const browser = await playwright.chromium.launch({
-    args: chromium.args,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
-  });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  // Set page UA
+  await setPageUserAgent(page);
+  // Remove default timeout
+  page.setDefaultNavigationTimeout(0);
   // Go to Instagram
   await page.goto('https://www.instagram.com/');
   console.log('go to instagram');
@@ -118,50 +110,55 @@ const getFollowersFromInstagram = async (
   await page.click('button[type="submit"]');
   // Wait for login success redirect
   await page.waitForNavigation({
-    waitUntil: 'domcontentloaded',
+    waitUntil: 'networkidle2',
   });
   console.log('login success');
   // Go to profile
-  await page.goto(`https://www.instagram.com/${username}/`);
-  // Get number of followers
-  const followers: number = await getNumberOfFollowersFromPage(page);
-  if (followers > 0) {
-    // Open followers list
-    await page.click("a[href*='/followers']");
+  await page.goto(`https://www.instagram.com/${username}/`, {
+    waitUntil: 'networkidle2',
+  });
+  await page.waitForSelector("a[href*='/followers']");
+  // Open followers list
+  await page.click("a[href*='/followers']");
+  try {
     await page.waitForSelector('div[aria-label="Followers"] ul li');
-    // Get the followers list
-    await loadFullFollowersList(page, followers);
-    const followerUsernames = await getUsernamesFromFollowersList(page);
-    console.log('followerUsernames length', followerUsernames.length);
-    // Get each follower's profile info
-    for (const followerUsername of followerUsernames) {
-      const profilePage = await browser.newPage();
-      try {
-        // Go to profile page
-        await profilePage.goto(
-          `https://www.instagram.com/${followerUsername}/`,
-        );
-        // Get number of followers
-        const followerCount = await getNumberOfFollowersFromPage(profilePage);
-        // Close page
-        await profilePage.close();
-        // Update followersInfo
-        console.log('new follower info added', {
-          username: followerUsername,
-          follower_count: followerCount,
-        });
-        followersInfo.push({
-          username: followerUsername,
-          follower_count: followerCount,
-        });
-      } catch (error) {
-        console.log(`failed to fetch profile from ${followerUsername}`, error);
-        // Close page
-        await profilePage.close();
-        continue;
-      }
+  } catch {
+    // No followers
+    return [];
+  }
+  // Get the followers list
+  await loadFullFollowersList(page);
+  const followerUsernames = await getUsernamesFromFollowersList(page);
+  console.log('followerUsernames length', followerUsernames.length);
+  // Get each follower's profile info
+  for (const followerUsername of followerUsernames) {
+    const profilePage = await browser.newPage();
+    // Set page UA
+    await setPageUserAgent(profilePage);
+    try {
+      // Go to profile page
+      await profilePage.goto(`https://www.instagram.com/${followerUsername}/`);
+      // Get number of followers
+      const followerCount = await getNumberOfFollowersFromPage(profilePage);
+      // Close page
+      await profilePage.close();
+      // Update followersInfo
+      console.log('new follower info added', {
+        username: followerUsername,
+        follower_count: followerCount,
+      });
+      followersInfo.push({
+        username: followerUsername,
+        follower_count: followerCount,
+      });
+    } catch (error) {
+      console.log(`failed to fetch profile from ${followerUsername}`, error);
+      // Close page
+      await profilePage.close();
+      continue;
     }
   }
+
   // Close browser
   await browser.close();
 
